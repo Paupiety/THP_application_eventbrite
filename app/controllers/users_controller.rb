f523e7f7b6d62084ada189a1ae30d09642bb5b27
paupiety <manonesqu@gmail.com>
2024-05-07 13:10:02 +0200

    ajout de devise et test de toutes les pages authentification/connexion + test sur fly : OK juste le seed ne fonctionne pas sur fly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
+    #before_action :authenticate_user!
     
     def show()
-        id = params[:id]
-        @user = User.find(id)    
+            id = params[:id]
+            @user = User.find(id)
+        unless @user
+            redirect_to new_user_session_path
+            flash[:error] = "Tu dois être connecter pour accéder aux détails des événements"
+        end
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     after_create :welcome_send
 
     validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "Une adresse valide s'il vous paît" }
-    validates :description, presence: true, length: {in: 20..1000}
+    validates :description, length: {in: 20..1000}, on: :update
     validates :first_name, presence: true
     validates :last_name, presence: true
     has_many :attendances
